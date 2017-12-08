139b457e6543561f5af227a0e418e4fc825aa0ba
Jade Allen Cook <jcook@deseretnews.com>
2017-12-08 14:19:13 -0700

    ⚙🔥 Refactored code to use classes

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
-(() => {
-  // show selected homicide
-  function displayHomicide(homicide) {
-    const photo = document.getElementById('current-homicide-photo'),
-      name = document.getElementById('current-homicide-name'),
-      summary = document.getElementById('current-homicide-summary');
-    photo.style.backgroundImage = 'url(' + getPhotoURL(homicide.image) + ')';
-    name.innerText = homicide.first + ' ' + homicide.last;
-    summary.innerText = homicide.summary;
+new class HomicideApp {
+  // init build
+  constructor() {
+    // cache build elems
+    this.elems = {
+      photo: document.getElementById('current-homicide-photo'),
+      name: document.getElementById('current-homicide-name'),
+      summary: document.getElementById('current-homicide-summary'),
+      victims: document.getElementById('homicide-victims')
+    }
+    // init process
+    this.showVictims();
+    this.showHomicide(homicides[0]);
   }
-  // show results 
-  function displayVictims() {
-    const container = document.getElementById('homicide-victims');
-    container.innerHTML = '';
+  // show selected homicide
+  showHomicide(homicide) {
+    this.elems.photo.style.backgroundImage = 'url(' + this.getPhotoURL(homicide.image) + ')';
+    this.elems.name.innerText = homicide.first + ' ' + homicide.last;
+    this.elems.summary.innerText = homicide.summary;
+  }
+  // show victim results
+  showVictims(search) {
+    this.elems.victims.innerHTML = '';
     for (let x = 0; x < 6; x++) {
       let html = '<div class="col-2">';
-      html += '<div class="victim-image" style="background-image: url(' + getPhotoURL(homicides[x].image) + ')"></div>';
+      html += '<div class="victim-image" style="background-image: url(' + this.getPhotoURL(homicides[x].image) + ')"></div>';
       html += '</div>'
-      container.innerHTML += html;
+      this.elems.victims.innerHTML += html;
     }
   }
-  // get photo from google drive
-  function getPhotoURL(url) {
+  // get photo url from google
+  getPhotoURL(url) {
     const id = url.replace('https://drive.google.com/open?id=', '');
     return 'https://drive.google.com/a/deseretnews.com/uc?export=view&id=' + id;
   }
-  // show first homicide
-  displayVictims();
-  displayHomicide(homicides[0]);
-})();
+}
