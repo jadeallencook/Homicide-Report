0e1a8ab0128290cbc8e8c876a63681befedca508
Jade Allen Cook <jcook@deseretnews.com>
2017-12-08 15:23:58 -0700

    🗺 Result now shows in Google Map

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-new class HomicideApp {
+class HomicideApp {
   // init build
   constructor() {
     // cache build elems
@@ -6,31 +6,58 @@ new class HomicideApp {
       photo: document.getElementById('current-homicide-photo'),
       name: document.getElementById('current-homicide-name'),
       summary: document.getElementById('current-homicide-summary'),
-      victims: document.getElementById('homicide-victims')
+      victims: document.getElementById('homicide-victims'),
+      map: document.getElementById('homicide-map')
     }
     // init process
-    this.showVictims();
+    this.showVictims(homicides);
     this.showHomicide(homicides[0]);
   }
   // show selected homicide
   showHomicide(homicide) {
+    // set bio section
     this.elems.photo.style.backgroundImage = 'url(' + this.getPhotoURL(homicide.image) + ')';
     this.elems.name.innerText = homicide.first + ' ' + homicide.last;
     this.elems.summary.innerText = homicide.summary;
+    // reposition map
+    const lat = parseFloat(homicide.location.substr(0, homicide.location.indexOf(','))),
+      lng = parseFloat(homicide.location.split(',')[1]);
+    this.createMap({
+      lat: lat,
+      lng: lng
+    });
   }
   // show victim results
-  showVictims(search) {
+  showVictims(results) {
+    if (!results) results = homicides;
     this.elems.victims.innerHTML = '';
-    for (let x = 0; x < 6; x++) {
-      let html = '<div class="col-2">';
-      html += '<div class="victim-image" style="background-image: url(' + this.getPhotoURL(homicides[x].image) + ')"></div>';
-      html += '</div>'
-      this.elems.victims.innerHTML += html;
+    let html = '';
+    for (let x = 0, max = --results.length; x < max; x++) {
+      html += '<div class="col-2">';
+      html += '<div class="victim-image" style="background-image: url(' + this.getPhotoURL(results[x].image) + ')"></div>';
+      html += '</div>';
     }
+    this.elems.victims.innerHTML += html;
+  }
+  // generate google map
+  createMap(coordinates) {
+    const map = new google.maps.Map(this.elems.map, {
+      zoom: 14,
+      center: coordinates
+    });
+    const marker = new google.maps.Marker({
+      position: coordinates,
+      map: map
+    });
   }
   // get photo url from google
   getPhotoURL(url) {
     const id = url.replace('https://drive.google.com/open?id=', '');
     return 'https://drive.google.com/a/deseretnews.com/uc?export=view&id=' + id;
   }
+}
+
+// callback for google api
+function initHomicideApp() {
+  new HomicideApp();
 }
