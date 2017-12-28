375abbe68605383f6cf47338384ed25c58537376
Jade Allen Cook <jcook@deseretnews.com>
2017-12-28 12:17:41 -0700

    ⛽️ Ran data analysis using new data set

--- js/types.js
+++ js/types.js
@@ -1,3 +1,5 @@
+// 15 - 21
+
 clear();
 types = {};
 for (let homicide of homicides) {
@@ -5,10 +7,23 @@ for (let homicide of homicides) {
 		types[homicide.type] = {
 			num: 1,
 			cases: [homicide]
-        }
-    } else if (types[homicide.type] && homicide.age > 14 && homicide.age < 22) {
+		}
+	} else if (types[homicide.type] && homicide.age > 14 && homicide.age < 22) {
 		types[homicide.type].num++;
 		types[homicide.type].cases.push(homicide);
-    }
+	}
+}
+console.log(types);
+
+// all 
+
+clear();
+types = {};
+for (let homicide of homicides) {
+	if (!types[homicide.type]) {
+		types[homicide.type] = 1;
+	} else {
+		types[homicide.type]++;
+	}
 }
 console.log(types);
