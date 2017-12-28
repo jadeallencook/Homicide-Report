80b173fdd392b264b80881eb55edeff7a3c24215
Jade Allen Cook <jcook@deseretnews.com>
2017-12-28 09:34:19 -0700

    🐛 Fixed bug that cause wrong type calculation

--- js/types.js
+++ js/types.js
@@ -1,10 +1,14 @@
+clear();
 types = {};
 for (let homicide of homicides) {
 	if (!types[homicide.type] && homicide.age > 14 && homicide.age < 22) {
-		types[homicide.type] = 1;
-    } else {
-		types[homicide.type]++;
+		types[homicide.type] = {
+			num: 1,
+			cases: [homicide]
+        }
+    } else if (types[homicide.type] && homicide.age > 14 && homicide.age < 22) {
+		types[homicide.type].num++;
+		types[homicide.type].cases.push(homicide);
     }
 }
-clear();
 console.log(types);
